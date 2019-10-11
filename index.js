c2961308f09d0dbc4f8060a39c259224a12e15f5
liudaojie <liudaojie@yy.com>
2019-10-11 19:20:11 +0800

    feat: 添加 test case

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ class YylConcatWebpackPlugin {
       'yylConcat',
       (compilation, done) => {
         Object.keys(compilation.assets).forEach((key) => {
-          let cnt = compilation.assets[key].source();
+          let cnt = compilation.assets[key].source()
           if (typeof cnt === 'string') {
             // cnt = cnt.replace(/(\w+)\.default\(/g, '$1[\'default\'](');
             console.log('===', key)
             compilation.assets[key] = {
               source() {
-                return cnt;
+                return cnt
               },
               size() {
-                return cnt.length;
+                return cnt.length
               }
-            };
+            }
           }
-        });
-        done();
+        })
+        done()
       }
-    );
+    )
   }
 }
 
-module.exports = YylConcatWebpackPlugin;
+module.exports = YylConcatWebpackPlugin
